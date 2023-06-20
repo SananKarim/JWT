a3da1700c7b36f102bdc708753b003697f123339
SananKarim <65340880+SananKarim@users.noreply.github.com>
2023-06-20 11:54:28 +0500

    first commit JWT

---
+++ controllers/authController.js
@@ -0,0 +1,21 @@
+module.exports.signup_get = (req, res) => {
+  res.render("signup");
+};
+
+module.exports.login_get = (req, res) => {
+  res.render("login");
+};
+
+module.exports.signup_post = (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+  res.send("new signup");
+};
+
+module.exports.login_post = (req, res) => {
+  // console.log(req.body); //app.use(express.json()); in app.js
+  const { email, password } = req.body;
+  console.log(email);
+  console.log(password);
+  res.send("user login");
+};
