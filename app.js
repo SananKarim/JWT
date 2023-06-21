73afdbed399dd4c5aa6760677d7dd767de79693f
SananKarim <65340880+SananKarim@users.noreply.github.com>
2023-06-22 01:34:21 +0500

    cookies basic

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
@@ -7,6 +9,7 @@ const app = express();
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
@@ -25,6 +28,24 @@ mongoose
 
 // routes
 app.get("/", (req, res) => res.render("home"));
+
 app.get("/smoothies", (req, res) => res.render("smoothies"));
+
+//cookies
+app.get("/set-cookies", (req, res) => {
+  // res.setHeader("Set-Cookie", "newUser=true");
+
+  res.cookie("newUser", false);
+  res.cookie("isEmployee", true, { maxAge: 1000 * 60 * 60 * 24 }); //secure: true can also be added. this will only allow for HTTPS. Always use this in production.
+
+  res.send("you got the cookie");
+});
+
+app.get("/read-cookies", (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+
+  res.json(cookies);
+});
 
 app.use(authRoutes);
