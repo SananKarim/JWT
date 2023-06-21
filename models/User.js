2b5295500e25f147ab242f9b40067352efaefac4
SananKarim <65340880+SananKarim@users.noreply.github.com>
2023-06-21 15:31:51 +0500

    hooks

--- models/User.js
+++ models/User.js
@@ -3,13 +3,26 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    unique:true,
+    unique: true,
     required: true,
   },
   password: {
     type: String,
     required: true,
   },
+});
+
+//This post doesn't refers to post requests. It refers to specific event occured in userSchema and call this function after that event.
+//first argument action perfermed and second is a function. That function will be fired when
+userSchema.post("save", function (doc, next) {
+  console.log("new user created", doc);
+  next();
+});
+
+//before an action is saved in db
+userSchema.pre("save", function (next) {
+  console.log("user about to be created", this);
+  next();
 });
 
 const User = mongoose.model("user", userSchema);
